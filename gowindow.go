11dd74757f3cc246f82310a02a4398f09cf6ad31
yut-kt <32399978+yut-kt@users.noreply.github.com>
2021-09-09 22:26:21 +0900

    V0.1.1 (#5)
    * Feature/add window func (#3)
    
    * fix: hamming func test
    
    * add: rectangular window
    
    * add: triangular
    
    * update: ignore main
    
    * add: parzen window
    
    * add: deLaValleePoussin window
    
    * update: change how to apply window (#4)

---
+++ gowindow.go
@@ -0,0 +1,90 @@
+package gowindow
+
+type window int
+
+const (
+	// Rectangular https://en.wikipedia.org/wiki/Window_function#Rectangular_window
+	Rectangular window = iota
+	// Triangular https://en.wikipedia.org/wiki/Window_function#Triangular_window
+	Triangular
+	// Bartlett https://en.wikipedia.org/wiki/Window_function#Triangular_window
+	Bartlett
+	// Fejer https://en.wikipedia.org/wiki/Window_function#Triangular_window
+	Fejer
+	// Parzen https://en.wikipedia.org/wiki/Window_function#Parzen_window
+	Parzen
+	// DeLaValleePoussin https://en.wikipedia.org/wiki/Window_function#Parzen_window
+	DeLaValleePoussin
+	// Hanning https://en.wikipedia.org/wiki/Window_function#Hann_and_Hamming_windows
+	Hanning
+	// Hann https://en.wikipedia.org/wiki/Window_function#Hann_and_Hamming_windows
+	Hann
+	// Hamming https://en.wikipedia.org/wiki/Window_function#Hann_and_Hamming_windows
+	Hamming
+	// Blackman https://en.wikipedia.org/wiki/Window_function#Blackman_window
+	Blackman
+)
+
+// Apply func to apply window func Destructively
+func Apply(s []float64, w window) {
+	chooseApplyFunc(w)(s)
+}
+
+func chooseApplyFunc(w window) func([]float64) {
+	switch w {
+	case Rectangular:
+		return rectangular
+	case Triangular:
+		return triangular
+	case Bartlett:
+		return bartlett
+	case Fejer:
+		return fejer
+	case Parzen:
+		return parzen
+	case DeLaValleePoussin:
+		return deLaValleePoussin
+	case Hanning:
+		return hanning
+	case Hann:
+		return hann
+	case Hamming:
+		return hamming
+	case Blackman:
+		return blackman
+	}
+	// return empty if unknown window
+	return func(float64s []float64) {}
+}
+
+// ApplyNew func to apply window func Non-Destructively
+func ApplyNew(s []float64, w window) []float64 {
+	return chooseApplyNewFunc(w)(s)
+}
+
+func chooseApplyNewFunc(w window) func([]float64) []float64 {
+	switch w {
+	case Rectangular:
+		return rectangularNew
+	case Triangular:
+		return triangularNew
+	case Bartlett:
+		return bartlettNew
+	case Fejer:
+		return fejerNew
+	case Parzen:
+		return parzenNew
+	case DeLaValleePoussin:
+		return deLaValleePoussinNew
+	case Hanning:
+		return hanningNew
+	case Hann:
+		return hannNew
+	case Hamming:
+		return hammingNew
+	case Blackman:
+		return blackmanNew
+	}
+	// return empty if unknown window
+	return func(float64s []float64) []float64 { return []float64{} }
+}
---
+++ rectangular.go
@@ -0,0 +1,8 @@
+package gowindow
+
+// Rectangular is func to apply Rectangular Window
+func rectangular(s []float64) {}
+
+func rectangularNew(s []float64) []float64 {
+	return s
+}
